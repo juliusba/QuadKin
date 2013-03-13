47951c6ea74c7013eb2bfa4981eef687edb1498e
gautegam <gaute@gamnes.no>
2013-03-13 13:28:40 +0100

    Gaute testing

--- QuadKin/QuadKin/Quad/NavDataWorker.cs
+++ QuadKin/QuadKin/Quad/NavDataWorker.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+
+//Gaute
 
 namespace QuadKin.Quad
 {
